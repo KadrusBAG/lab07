bd7553a7dca1fdd744f948ed3b8a19604f6e2dfd
KadrusBAG <79163891145@ya.ru>
2018-04-19 10:51:05 +0300

    added documentation

--- include/print.hpp
+++
@@ -1,7 +0,0 @@
-
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout);  //вывод в консоль
-void print(const std::string& text, std::ofstream& out);  //вывод в файл
